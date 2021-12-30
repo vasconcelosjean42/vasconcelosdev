56b8e1ade82672d29b98e30688cb82ff5e23b294
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2021-12-30 13:03:56 -0300

    tela de sobre completa - sidebar fixada - elementos ajustados

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Card from "../../components/Card";
 import Infos from "../../components/Infos";
-import Stars from "../../components/Stars";
 import data from "./data";
 import { Container } from "./styles";
 
