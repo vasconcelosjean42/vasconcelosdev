a1216fd1e7df899e53b5394d45d132239fb792fa
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2022-01-04 17:27:13 -0300

    stars colors

--- src/components/Stars/index.tsx
+++ src/components/Stars/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaStar } from "react-icons/fa";
+import { ThemeContext } from "styled-components";
 import { Container } from "./style";
 
 type Props = {
@@ -8,11 +9,13 @@ type Props = {
 
 const Stars = ({ amount }: Props) => {
     const rows = [];
+    const { colors } = useContext(ThemeContext);
+
     for (let i = 0; i < amount; i++) {
         rows.push(<FaStar className="icon" />)
     }
     for (let i = 0; i < 5 - amount; i++) {
-        rows.push(<FaStar style={{ width: "1.1rem", color: "${props => props.theme.colors.primary}" }} opacity={0.5} />)
+        rows.push(<FaStar style={{ width: "1.1rem", color: colors.primary }} opacity={0.5} />)
     }
 
     return (
